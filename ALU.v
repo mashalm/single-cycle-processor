92431ddd9357b97118857af0deedfd9b713c3a56
mashalm <mashal.mannan@gmail.com>
2015-10-21 00:09:46 -0400

    finished ALU, new DataMem

--- ALU.v
+++ ALU.v
@@ -1,44 +1,121 @@
-module ALU(func, dataIn1, dataIn2, dataOut);
+module ALU(func, dataIn1, dataIn2, dataOut, compTrue);
 
 	input[31:0] dataIn1, dataIn2;
 	input[4:0] func; //one bit for whether this is a comp or not
+	//so func = {compOrNot, iword[7:4]}
 	
 	output[31:0] dataOut;
+	output compTrue;
+	reg compTrue;
 	reg[31:0] dataOut;
+	
 	
 	always@(*) begin //for combinational logic
 		case(func) begin
-		5'b00000: begin //ADD or ADDI or LD or ST - add imm for those
+		5'b00000: begin //ADD or ADDI or LD or ST - add imm for those, or JAL
 					dataOut <= dataIn1 + dataIn2;
+					compTrue <= 1'b0; //no comparison happening
 					end;
 		5'b00001: begin //SUB or SUBI
 					dataOut <= dataIn1 - dataIn2;
+					compTrue <= 1'b0;
 					end;
 		5'b00100: begin //AND or ANDI
 					dataOut <= dataIn1 & dataIn2;
+					compTrue <= 1'b0;
 					end;
 		5'b00101: begin //OR or ORI
 					dataOut <= dataIn1 | dataIn2;
+					compTrue <= 1'b0;
 					end;
-		5'b00110:begin // XOR or XORI
+		5'b00110: begin // XOR or XORI
 					dataOut <= dataIn1 ^ dataIn2;
+					compTrue <= 1'b0;
 					end
-		5'b01100:begin // NAND or NANDI
+		5'b01100: begin // NAND or NANDI
 					dataOut <= ~(dataIn1 & dataIn2);
+					compTrue <= 1'b0;
 					end
-		5'b01101:begin // NOR or NORI
+		5'b01101: begin // NOR or NORI
 					dataOut <= ~(dataIn1 | dataIn2);
+					compTrue <= 1'b0;
 					end
-		5'b01110:begin // XNOR or XNORI
+		5'b01110: begin // XNOR or XNORI
 					dataOut <= ~(dataIn1 ^ dataIn2);
+					compTrue <= 1'b0;
 					end
-		5'b01011:begin // MVHI
+		5'b01011: begin // MVHI
 					dataOut <= ((dataIn2 & 32'hFFFF0000));
+					compTrue <= 1'b0;
+					end
+		//below are the comparing instructions
+		//so func = {1, iword[7:4]}
+		5'b10000: begin //F or FI or BF
+					dataOut <= 32'd0;
+					compTrue <= 1'b0; //comparison is not "true"
+					//what is the purpose of BF??
+					end
+		5'b10001: begin //EQ or EQI or BEQ
+					dataOut <= (dataIn1 == dataIn2) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 == dataIn2) ? 1'b1 : 1'b0;
+					end
+		5'b10010: begin //LT or LTI or BLT
+					dataOut <= (dataIn1 < dataIn2) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 < dataIn2) ? 1'b1 : 1'b0;
+					end
+		5'b10011: begin //LTE or LTEI or BLTE
+					dataOut <= (dataIn1 <= dataIn2) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 <= dataIn2) ? 1'b1 : 1'b0;
+					end
+		5'b11000: begin //T or TI or BT
+					dataOut <= 32'd1;
+					compTrue <= 1'b1;
+					end
+		5'b11001: begin //NE or NEI or BNE
+					dataOut <= (dataIn1 != dataIn2) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 != dataIn2) ? 1'b1 : 1'b0;
+					end
+		5'b11010: begin //GTE or GTEI or BGTE
+					dataOut <= (dataIn1 >= dataIn2) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 >= dataIn2) ? 1'b1 : 1'b0;
+					end
+		5'b11011: begin //GT or GTI or BGT
+					dataOut <= (dataIn1 > dataIn2) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 > dataIn2) ? 1'b1 : 1'b0;
 					end
 					
-		//implement cmp, branch and ld/st
-		 
-		 
+		//branch instructions below
+		// func still = {1, iword[7:4]}
+		//still need BEQZ, BLTZ, BLTEZ, BNEZ, BGTEZ, BGTZ
+		5'b10101: begin //BEQZ
+					//there is no RD so it doesn't matter what we output to dataOut?
+					dataOut <= (dataIn1 == 32'd0) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 == 32'd0) ? 1'b1 : 1'b0;
+					end
+		5'b10110: begin //BLTZ
+					dataOut <= (dataIn1 < 32'd0) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 < 32'd0) ? 1'b1 : 1'b0;
+					end
+		5'b10111: begin //BLTEZ
+					dataOut <= (dataIn1 <= 32'd0) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 <= 32'd0) ? 1'b1 : 1'b0;
+					end
+		5'b11101: begin //BNEZ
+					dataOut <= (dataIn1 != 32'd0) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 != 32'd0) ? 1'b1 : 1'b0;
+					end
+		5'b11110: begin //BGTEZ
+					dataOut <= (dataIn1 >= 32'd0) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 >= 32'd0) ? 1'b1 : 1'b0;
+					end
+		5'b11111: begin //BGTZ
+					dataOut <= (dataIn1 > 32'd0) ? 32'd1 : 32'd0;
+					compTrue <= (dataIn1 > 32'd0) ? 1'b1 : 1'b0;
+					end
+		default: begin //required, output whatever?
+					dataOut <= 32'd0;
+					compTrue <= 1'b0;
+
 		 endcase
    end
 endmodule
--- DataMemory.v
+++ DataMemory.v
@@ -1,20 +1,43 @@
-module DataMemory(clk, wrtEn, dIn, addrIn, dOut);
-
-//parameters ----- look at iMem
+module DataMemory(clk, wrtEn, addr, dIn, sw, key, ledr, ledg, hex, dOut);
+	parameter MEM_INIT_FILE;
+	parameter ADDR_BIT_WIDTH = 32;
+	parameter DATA_BIT_WIDTH = 32;
+	parameter TRUE_ADDR_BIT_WIDTH = 11;
+	parameter N_WORDS = (1 << TRUE_ADDR_BIT_WIDTH);
 
 	input clk, wrtEn;
-	input[31:0] dIn;
-	input[10:0] addrIn; //only 11 bits
-	output[31:0] dOut;
-	
-	reg[31: 0] data[0: 10];
+	input [3:0] key;
+	input [9:0] sw;
+	input [ADDR_BIT_WIDTH - 1 : 0] addr;
+	input [DATA_BIT_WIDTH - 1 : 0] dIn;
+	output [DATA_BIT_WIDTH - 1 : 0] dOut;
+	output reg [9:0] ledr;
+	output reg [7:0] ledg;
+	output reg [15:0] hex;
 
-	assign dOut = data[addr];
-	
-	always @ (posedge clk) begin
-		if(wrtEn == 1'b1) //for sw
-			data[addr] = dIn;
+	(* ram_init_file = MEM_INIT_FILE *)
+	reg [DATA_BIT_WIDTH - 1 : 0] data [0 : N_WORDS - 1];
+	reg [0 : N_WORDS - 1] addr_reg;
+	reg [DATA_BIT_WIDTH - 1 : 0] sw_reg;
+	reg [DATA_BIT_WIDTH - 1 : 0] key_reg;
+
+	always @(posedge clk) begin
+		if (addr[29]) begin
+			if (wrtEn) begin
+	  			if (addr[2]) ledr <= dIn[9:0];
+				else if (addr[3]) ledg <= dIn[7:0];
+				else hex <= dIn[15:0];
+	      	end else begin
+	      		if (addr[2]) sw_reg <= sw;
+	      		else key_reg <= (~key & 4'hf);
+	      	end
+      	end
+    end
+
+	always @(negedge clk) begin
+		if (wrtEn && !addr[29]) data[addr[13:2]] <= dIn;
+		addr_reg <= addr[13:2];
 	end
-	
+
+	assign dOut = addr[29] ? (addr[2] ? sw_reg : key_reg) : data[addr_reg];
 endmodule
-	
