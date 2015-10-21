6048d4f9509204d62d986b326fbeb4887c3d930a
mashalm <mashal.mannan@gmail.com>
2015-10-21 18:14:36 -0400

    added skeleton for controller

--- ALU.v
+++ ALU.v
@@ -12,110 +12,110 @@ module ALU(func, dataIn1, dataIn2, dataOut, compTrue);
 	
 	always@(*) begin //for combinational logic
 		case(func) begin
-		5'b00000: begin //ADD or ADDI or LD or ST - add imm for those, or JAL
-					dataOut <= dataIn1 + dataIn2;
-					compTrue <= 1'b0; //no comparison happening
-					end;
-		5'b00001: begin //SUB or SUBI
-					dataOut <= dataIn1 - dataIn2;
-					compTrue <= 1'b0;
-					end;
-		5'b00100: begin //AND or ANDI
-					dataOut <= dataIn1 & dataIn2;
-					compTrue <= 1'b0;
-					end;
-		5'b00101: begin //OR or ORI
-					dataOut <= dataIn1 | dataIn2;
-					compTrue <= 1'b0;
-					end;
-		5'b00110: begin // XOR or XORI
-					dataOut <= dataIn1 ^ dataIn2;
-					compTrue <= 1'b0;
-					end
-		5'b01100: begin // NAND or NANDI
-					dataOut <= ~(dataIn1 & dataIn2);
-					compTrue <= 1'b0;
-					end
-		5'b01101: begin // NOR or NORI
-					dataOut <= ~(dataIn1 | dataIn2);
-					compTrue <= 1'b0;
-					end
-		5'b01110: begin // XNOR or XNORI
-					dataOut <= ~(dataIn1 ^ dataIn2);
-					compTrue <= 1'b0;
-					end
-		5'b01011: begin // MVHI
-					dataOut <= ((dataIn2 & 32'hFFFF0000));
-					compTrue <= 1'b0;
-					end
-		//below are the comparing instructions
-		//so func = {1, iword[7:4]}
-		5'b10000: begin //F or FI or BF
-					dataOut <= 32'd0;
-					compTrue <= 1'b0; //comparison is not "true"
-					//what is the purpose of BF??
-					end
-		5'b10001: begin //EQ or EQI or BEQ
-					dataOut <= (dataIn1 == dataIn2) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 == dataIn2) ? 1'b1 : 1'b0;
-					end
-		5'b10010: begin //LT or LTI or BLT
-					dataOut <= (dataIn1 < dataIn2) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 < dataIn2) ? 1'b1 : 1'b0;
-					end
-		5'b10011: begin //LTE or LTEI or BLTE
-					dataOut <= (dataIn1 <= dataIn2) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 <= dataIn2) ? 1'b1 : 1'b0;
-					end
-		5'b11000: begin //T or TI or BT
-					dataOut <= 32'd1;
-					compTrue <= 1'b1;
-					end
-		5'b11001: begin //NE or NEI or BNE
-					dataOut <= (dataIn1 != dataIn2) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 != dataIn2) ? 1'b1 : 1'b0;
-					end
-		5'b11010: begin //GTE or GTEI or BGTE
-					dataOut <= (dataIn1 >= dataIn2) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 >= dataIn2) ? 1'b1 : 1'b0;
-					end
-		5'b11011: begin //GT or GTI or BGT
-					dataOut <= (dataIn1 > dataIn2) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 > dataIn2) ? 1'b1 : 1'b0;
-					end
-					
-		//branch instructions below
-		// func still = {1, iword[7:4]}
-		//still need BEQZ, BLTZ, BLTEZ, BNEZ, BGTEZ, BGTZ
-		5'b10101: begin //BEQZ
-					//there is no RD so it doesn't matter what we output to dataOut?
-					dataOut <= (dataIn1 == 32'd0) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 == 32'd0) ? 1'b1 : 1'b0;
-					end
-		5'b10110: begin //BLTZ
-					dataOut <= (dataIn1 < 32'd0) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 < 32'd0) ? 1'b1 : 1'b0;
-					end
-		5'b10111: begin //BLTEZ
-					dataOut <= (dataIn1 <= 32'd0) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 <= 32'd0) ? 1'b1 : 1'b0;
-					end
-		5'b11101: begin //BNEZ
-					dataOut <= (dataIn1 != 32'd0) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 != 32'd0) ? 1'b1 : 1'b0;
-					end
-		5'b11110: begin //BGTEZ
-					dataOut <= (dataIn1 >= 32'd0) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 >= 32'd0) ? 1'b1 : 1'b0;
-					end
-		5'b11111: begin //BGTZ
-					dataOut <= (dataIn1 > 32'd0) ? 32'd1 : 32'd0;
-					compTrue <= (dataIn1 > 32'd0) ? 1'b1 : 1'b0;
-					end
-		default: begin //required, output whatever?
-					dataOut <= 32'd0;
-					compTrue <= 1'b0;
+			5'b00000: begin //ADD or ADDI or LD or ST - add imm for those, or JAL
+						dataOut <= dataIn1 + dataIn2;
+						compTrue <= 1'b0; //no comparison happening
+						end;
+			5'b00001: begin //SUB or SUBI
+						dataOut <= dataIn1 - dataIn2;
+						compTrue <= 1'b0;
+						end;
+			5'b00100: begin //AND or ANDI
+						dataOut <= dataIn1 & dataIn2;
+						compTrue <= 1'b0;
+						end;
+			5'b00101: begin //OR or ORI
+						dataOut <= dataIn1 | dataIn2;
+						compTrue <= 1'b0;
+						end;
+			5'b00110: begin // XOR or XORI
+						dataOut <= dataIn1 ^ dataIn2;
+						compTrue <= 1'b0;
+						end
+			5'b01100: begin // NAND or NANDI
+						dataOut <= ~(dataIn1 & dataIn2);
+						compTrue <= 1'b0;
+						end
+			5'b01101: begin // NOR or NORI
+						dataOut <= ~(dataIn1 | dataIn2);
+						compTrue <= 1'b0;
+						end
+			5'b01110: begin // XNOR or XNORI
+						dataOut <= ~(dataIn1 ^ dataIn2);
+						compTrue <= 1'b0;
+						end
+			5'b01011: begin // MVHI
+						dataOut <= ((dataIn2 & 32'hFFFF0000));
+						compTrue <= 1'b0;
+						end
+			//below are the comparing instructions
+			//so func = {1, iword[7:4]}
+			5'b10000: begin //F or FI or BF
+						dataOut <= 32'd0;
+						compTrue <= 1'b0; //comparison is not "true"
+						//what is the purpose of BF??
+						end
+			5'b10001: begin //EQ or EQI or BEQ
+						dataOut <= (dataIn1 == dataIn2) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 == dataIn2) ? 1'b1 : 1'b0;
+						end
+			5'b10010: begin //LT or LTI or BLT
+						dataOut <= (dataIn1 < dataIn2) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 < dataIn2) ? 1'b1 : 1'b0;
+						end
+			5'b10011: begin //LTE or LTEI or BLTE
+						dataOut <= (dataIn1 <= dataIn2) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 <= dataIn2) ? 1'b1 : 1'b0;
+						end
+			5'b11000: begin //T or TI or BT
+						dataOut <= 32'd1;
+						compTrue <= 1'b1;
+						end
+			5'b11001: begin //NE or NEI or BNE
+						dataOut <= (dataIn1 != dataIn2) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 != dataIn2) ? 1'b1 : 1'b0;
+						end
+			5'b11010: begin //GTE or GTEI or BGTE
+						dataOut <= (dataIn1 >= dataIn2) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 >= dataIn2) ? 1'b1 : 1'b0;
+						end
+			5'b11011: begin //GT or GTI or BGT
+						dataOut <= (dataIn1 > dataIn2) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 > dataIn2) ? 1'b1 : 1'b0;
+						end
+						
+			//branch instructions below
+			// func still = {1, iword[7:4]}
+			//still need BEQZ, BLTZ, BLTEZ, BNEZ, BGTEZ, BGTZ
+			5'b10101: begin //BEQZ
+						//there is no RD so it doesn't matter what we output to dataOut?
+						dataOut <= (dataIn1 == 32'd0) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 == 32'd0) ? 1'b1 : 1'b0;
+						end
+			5'b10110: begin //BLTZ
+						dataOut <= (dataIn1 < 32'd0) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 < 32'd0) ? 1'b1 : 1'b0;
+						end
+			5'b10111: begin //BLTEZ
+						dataOut <= (dataIn1 <= 32'd0) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 <= 32'd0) ? 1'b1 : 1'b0;
+						end
+			5'b11101: begin //BNEZ
+						dataOut <= (dataIn1 != 32'd0) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 != 32'd0) ? 1'b1 : 1'b0;
+						end
+			5'b11110: begin //BGTEZ
+						dataOut <= (dataIn1 >= 32'd0) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 >= 32'd0) ? 1'b1 : 1'b0;
+						end
+			5'b11111: begin //BGTZ
+						dataOut <= (dataIn1 > 32'd0) ? 32'd1 : 32'd0;
+						compTrue <= (dataIn1 > 32'd0) ? 1'b1 : 1'b0;
+						end
+			default: begin //required, output whatever?
+						dataOut <= 32'd0;
+						compTrue <= 1'b0;
 
-		 endcase
-   end
+		endcase
+   end //always
 endmodule
--- ScProcController.v
+++ ScProcController.v
@@ -1,1 +1,46 @@
-module SCProcController()
+module SCProcController(iword, aluCompTrue, aluFn, rdIndex0, rdIndex1, wrtIndex, imm, 
+regFileWrtEn, dMemWrtEn, aluSrc2Sel, PCSel, regFileWrtSel);
+
+//use parameters later
+
+	input[31:0] iword;
+	input aluCompTrue;
+	
+	output[4:0] aluFn;
+	output[3:0] rdIndex0, rdIndex1, wrtIndex;
+	output[15:0] imm;
+	output regFileWrtEn, dMemWrtEn;
+	output[1:0] PCSel, aluSrc2Sel, regFileWrtSel;
+	
+	always @(*) (begin
+		case(iword[31:28]) //look at primary opcode, output func
+			4'b0000: begin //ALU-R instruction
+			
+			end
+			4'b1000: begin //ALU-I instruction
+			
+			end
+			4'b1001: begin //LW instruction
+			
+			end
+			4'b0101: begin //SW instruction
+			
+			end
+			4'b0010: begin //CMP-R instruction
+			
+			end
+			4'b1010: begin //CPM-I instruction
+			
+			end
+			4'b0110: begin //BRANCH instruction
+			
+			end
+			4'b1011: begin //JAL instruction
+			
+			end
+		endcase
+	end
+	
+	
+	
+endmodule
